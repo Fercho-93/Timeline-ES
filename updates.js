18130ed3720198ddb008c4bc29ee30fba5910093
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-09 20:07:31 +0200

    chore: bump visible app version to v95

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v94";
+  CT.APP_VERSION = "continuum-v95";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
