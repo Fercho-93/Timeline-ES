45263cec6b9d430c6bce9cee944603e9a521acb2
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-09 20:57:13 +0200

    chore: actualizar beta Android a v104

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v103";
+  CT.APP_VERSION = "continuum-v104";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
