a610f0ba362c4a2b68577b990043b244a3c92c77
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-09 20:37:12 +0200

    chore: publicar correccion del temporizador

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v100";
+  CT.APP_VERSION = "continuum-v101";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
