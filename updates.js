70be43f44876458f14d767de082815cc68fb2d93
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-09 20:30:02 +0200

    chore: actualizar versión móvil a v99

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v98";
+  CT.APP_VERSION = "continuum-v99";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
