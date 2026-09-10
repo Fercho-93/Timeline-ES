5f917a3c6f89ce6b01300ffd24107d9099e23776
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-10 23:22:15 +0200

    Dar relieve de encuadernación suave a los mazos v126

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v125";
+  CT.APP_VERSION = "continuum-v126";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
