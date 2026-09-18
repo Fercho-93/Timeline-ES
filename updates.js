d8a186f4de2625757f8905652d9ace5a65c3dd27
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-18 11:50:54 +0200

    Asegura el boton circular de la guia en Safari

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v212";
+  CT.APP_VERSION = "continuum-v213";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
