970b593f54778eaa50c829868d7521fd7c6e411e
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-20 11:12:42 +0200

    Invalida la caché al publicar actualizaciones

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v263";
+  CT.APP_VERSION = "continuum-v265";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
