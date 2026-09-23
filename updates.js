4ea6f48e8d9bcfd38415e686a13fdd5ee11f9136
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-23 22:02:09 +0200

    Marca la nueva portada como v336

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v335";
+  CT.APP_VERSION = "continuum-v336";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
