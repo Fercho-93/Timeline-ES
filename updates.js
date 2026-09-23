4302a7350b1161b8b4287b70e8cc574d169106a0
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-23 22:55:37 +0200

    Marca la nueva ilustración como v341

--- updates.js
+++ updates.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
   const CT = window.CONTINUUM;
-  CT.APP_VERSION = "continuum-v340";
+  CT.APP_VERSION = "continuum-v341";
   CT.Updates = { start };
   function start() {
     if (!("serviceWorker" in navigator) || window.Capacitor?.isNativePlatform?.()) return;
