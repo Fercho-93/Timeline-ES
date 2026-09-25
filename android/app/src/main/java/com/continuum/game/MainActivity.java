dd1e77741aec326ba81e826747faa654e1fda10e
Fercho-93 <fernandosirventmerino@gmail.com>
2026-09-25 15:58:29 +0200

    prototype: register Android LAN signaling plugin

--- android/app/src/main/java/com/continuum/game/MainActivity.java
+++ android/app/src/main/java/com/continuum/game/MainActivity.java
@@ -1,5 +1,11 @@
 package com.continuum.game;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(LocalLanSignalPlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
